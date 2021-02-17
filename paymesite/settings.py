0378bfa7a2a32e5247559f28f2e8368cec14930f
Brendon Spiteri <brendon.spiteri.13@um.edu.mt>
2021-02-17 17:17:13 +0100

    Added  SSL security again

--- paymesite/settings.py
+++ paymesite/settings.py
@@ -107,8 +107,8 @@ ACCOUNT_EMAIL_REQUIRED = True
 SOCIALACCOUNT_EMAIL_REQUIRED  = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_LOGOUT_ON_GET = True
-#ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
-#SECURE_SSL_REDIRECT = False
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
+SECURE_SSL_REDIRECT = True
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
