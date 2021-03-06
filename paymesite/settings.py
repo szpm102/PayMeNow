8720edea9c8a014b73c84b9d4117554f5eec3633
Brendon Spiteri <brendon.spiteri.13@um.edu.mt>
2021-03-06 21:20:00 +0100

    Set Debug to false

--- paymesite/settings.py
+++ paymesite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_281q%ruv34r@veu_7npct#sg!0r=objk)1@&2g)_de!6+)d@b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
